583574e61b99cdeeeabf60caf3dd271964f1e85f
JAndreB <andrebyfield@live.co.uk>
2023-10-09 20:59:40 +0100

    ch3 excercises

---
+++ Ch3/greetings.py
@@ -0,0 +1,11 @@
+names = ['andre', 'wesley', 'myles', 'sharon']
+
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+
+print(f"Hi there {names[0].title()}, what would you like to do today?")
+print(f"Hi there {names[1].title()}, what would you like to do today?")
+print(f"Hi there {names[2].title()}, what would you like to do today?")
+print(f"Hi there {names[3].title()}, what would you like to do today?")
---
+++ Ch3/own_List.py
@@ -0,0 +1,6 @@
+List = ['bifrost', 'telekom', 'transporter', 'gigatrain']
+
+print(f"I would love to own a {List[0].title()}, some day")
+print(f"I would love to own a {List[1].title()}, some day")
+print(f"I would love to own a {List[2].title()}, some day")
+print(f"I would love to own a {List[3].title()}, some day")
